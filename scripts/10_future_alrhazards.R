c72c3e0e40da8cfeedb22436e0bc671ac97d04d1
mattwilliamson13 <mattwilliamson@boisestate.edu>
2023-05-28 11:13:02 -0600

    create hazard rst

---
+++ scripts/10_future_alrhazards.R
@@ -0,0 +1,29 @@
+library(tigris)
+library(sf)
+library(terra)
+library(tidyverse)
+temp <- tempfile()
+download.file("https://hazards.fema.gov/nri/Content/StaticDocuments/DataDownload//NRI_GDB_CensusTracts/NRI_GDB_CensusTracts.zip",temp)
+unzip(temp, exdir = here::here("data/original/future/"))
+unlink(temp)
+not.conus <- c("AK", "HI", "PR", "VI", "GU", "AS", "MP", "UM")
+conus.fips <- fips_codes %>% 
+  filter(., !state %in% not.conus) %>% 
+  dplyr::select(., state) 
+conus.fips.abbv <- unique(conus.fips$state)
+
+hazards <- st_read(here::here("data/original/future/NRI_GDB_CensusTracts.gdb/"), layer = "NRI_CensusTracts") %>% 
+  filter(., STATEABBRV %in% conus.fips.abbv)
+
+template.rast <- rast(here::here("data/processed/processed_state/us_rough.tif"))
+
+hazards.proj <- vect(hazards %>% st_transform(., crs=crs(template.rast)))
+
+alrag.rst <- rasterize(hazards.proj, template.rast, field="ALR_VALA")
+alrpop.rst <- rasterize(hazards.proj, template.rast, field="ALR_VALP")
+alrbldg.rst <- alrag.rst <- rasterize(hazards.proj, template.rast, field="ALR_VALB")
+
+
+writeRaster(alrag.rst, filename = here::here("data/processed/processed_future/alr_ag.tif"))
+writeRaster(alrpop.rst, filename = here::here("data/processed/processed_future/alr_pop.tif"))
+writeRaster(alrbldg.rst, filename = here::here("data/processed/processed_future/alr_bldg.tif"))
